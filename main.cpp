d3d9941506fee76ab740259ee370ad592cb3dbff
NickolAir <nikolay.ratushnyak03@gmail.com>
2023-04-28 22:17:06 +0700

    parallel part

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@
 #define Z0 -1
 #define Z1 1
 
-//Параметр уравнения
+//Главный параметр уравнения
 #define A 1e5
 
-//Порог сходимости
+//Предел сходимости
 #define E 1e-8
 
-//Начальное приближение
+//Изачальное приближение
 #define PHI0 0
 
 //Размеры сетки
@@ -41,11 +41,11 @@ double ro(double i, double j, double k) {
     return 6 - A * fi(i, j, k);
 }
 
-bool is_boundary_cell(int index, int bound) {
+bool isBoundaryElement(int index, int bound) {
     return index == 0 || index == bound - 1;
 }
 
-void grid_init(double *grid, double *previous_grid, int pr_cells_count, int pr_cells_shift) {
+void gridInit(double *grid, double *previous_grid, int pr_cells_count, int pr_cells_shift) {
     for (int i = 1; i < pr_cells_count + 1; i++) {
         for (int j = 0; j < NY; j++) {
             for (int k = 0; k < NZ; k++) {
@@ -54,7 +54,7 @@ void grid_init(double *grid, double *previous_grid, int pr_cells_count, int pr_c
 
                 int actual_i = i + pr_cells_shift;
 
-                if (is_boundary_cell(actual_i, NX) || is_boundary_cell(j, NY) || is_boundary_cell(k, NZ)) {
+                if (isBoundaryElement(actual_i, NX) || isBoundaryElement(j, NY) || isBoundaryElement(k, NZ)) {
                     grid[I(i, j, k)] = fi(actual_i, j, k);
                     previous_grid[I(i, j, k)] = fi(actual_i, j, k);
                 }
@@ -63,7 +63,7 @@ void grid_init(double *grid, double *previous_grid, int pr_cells_count, int pr_c
     }
 }
 
-double iteration_func(int i, int j, int k, double *grid, int pr_cells_shift) {
+double iterationFunc(int i, int j, int k, double *grid, int pr_cells_shift) {
     return ((1.0 / (2.0 / (hx * hx) + 2.0 / (hy * hy) + 2.0 / (hz * hz) + A)) *
             ((grid[I(i + 1, j, k)] + grid[I(i - 1, j, k)]) / (hx * hx) +
              (grid[I(i, j + 1, k)] + grid[I(i, j - 1, k)]) / (hy * hy) +
@@ -71,7 +71,7 @@ double iteration_func(int i, int j, int k, double *grid, int pr_cells_shift) {
              ro(i + pr_cells_shift, j, k)));
 }
 
-void update_previous_grid(int index, double *current_value, double *prev_value) {
+void updateGrid(int index, double *current_value, double *prev_value) {
     for (int j = 1; j < NY - 1; j++) {
         for (int k = 1; k < NZ - 1; k++) {
             prev_value[I(index, j, k)] = current_value[I(index, j, k)];
@@ -79,12 +79,12 @@ void update_previous_grid(int index, double *current_value, double *prev_value) 
     }
 }
 
-void update_grid_cell(int index, double *grid, double *previous_grid, int pr_cells_shift, double *pr_diff) {
+void updateGridCell(int index, double *grid, double *previous_grid, int pr_cells_shift, double *pr_diff) {
     int actual_index = index + pr_cells_shift;
-    if (!is_boundary_cell(actual_index, NX)) {
+    if (!isBoundaryElement(actual_index, NX)) {
         for (int j = 1; j < NY - 1; j++) {
             for (int k = 1; k < NZ - 1; k++) {
-                grid[I(index, j, k)] = iteration_func(index, j, k, previous_grid, pr_cells_shift);
+                grid[I(index, j, k)] = iterationFunc(index, j, k, previous_grid, pr_cells_shift);
 
                 double cur_diff = fabs(grid[I(index, j, k)] - previous_grid[I(index, j, k)]);
 
@@ -94,31 +94,31 @@ void update_grid_cell(int index, double *grid, double *previous_grid, int pr_cel
             }
         }
     }
-    update_previous_grid(index, grid, previous_grid);
+    updateGrid(index, grid, previous_grid);
 }
 
-void update_grid_center(double *grid, double *previous_grid, int pr_cells_count, int pr_cells_shift, double *pr_diff) {
+void updateGridCenter(double *grid, double *previous_grid, int pr_cells_count, int pr_cells_shift, double *pr_diff) {
     int center = (pr_cells_count + 1) / 2;
 
-    update_grid_cell(center, grid, previous_grid, pr_cells_shift, pr_diff);
+    updateGridCell(center, grid, previous_grid, pr_cells_shift, pr_diff);
 
     for (int j = 1; j < (pr_cells_count + 1) / 2; j++) {
-        update_grid_cell(center - j, grid, previous_grid, pr_cells_shift, pr_diff);
-        update_grid_cell(center + j, grid, previous_grid, pr_cells_shift, pr_diff);
+        updateGridCell(center - j, grid, previous_grid, pr_cells_shift, pr_diff);
+        updateGridCell(center + j, grid, previous_grid, pr_cells_shift, pr_diff);
     }
 
     if (pr_cells_count % 2 == 0) {
-        update_grid_cell(pr_cells_count, grid, previous_grid, pr_cells_shift, pr_diff);
+        updateGridCell(pr_cells_count, grid, previous_grid, pr_cells_shift, pr_diff);
     }
 }
 
-void update_grid_bound(double *grid, double *previous_grid, int pr_cells_count, int pr_cells_shift, double *pr_diff) {
-    update_grid_cell(1, grid, previous_grid, pr_cells_shift, pr_diff);
-    update_grid_cell(pr_cells_count, grid, previous_grid, pr_cells_shift, pr_diff);
+void updateGridBound(double *grid, double *previous_grid, int pr_cells_count, int pr_cells_shift, double *pr_diff) {
+    updateGridCell(1, grid, previous_grid, pr_cells_shift, pr_diff);
+    updateGridCell(pr_cells_count, grid, previous_grid, pr_cells_shift, pr_diff);
 }
 
-void send_grid_bound(double *grid, int pr_rank, int comm_size, int pr_cells_count, MPI_Request *request_prev,
-                     MPI_Request *request_next) {
+void sendGridBound(double *grid, int pr_rank, int comm_size, int pr_cells_count, MPI_Request *request_prev,
+                   MPI_Request *request_next) {
     int layer_size = NY * NZ;
     if (pr_rank != 0) {
         MPI_Isend(grid + layer_size, layer_size, MPI_DOUBLE, pr_rank - 1, 0, MPI_COMM_WORLD, &request_next[0]);
@@ -132,7 +132,7 @@ void send_grid_bound(double *grid, int pr_rank, int comm_size, int pr_cells_coun
     }
 }
 
-void recieve_grid_bound(int pr_rank, int comm_size, MPI_Request *request_prev, MPI_Request *request_next) {
+void recieveGridBound(int pr_rank, int comm_size, MPI_Request *request_prev, MPI_Request *request_next) {
     if (pr_rank != 0) {
         MPI_Wait(&request_next[0], MPI_STATUS_IGNORE);
         MPI_Wait(&request_next[1], MPI_STATUS_IGNORE);
@@ -143,9 +143,9 @@ void recieve_grid_bound(int pr_rank, int comm_size, MPI_Request *request_prev, M
     }
 }
 
-int jacobi_method(double *grid, int pr_cells_count, int pr_cells_shift) {
+int jacobyMethod(double *grid, int pr_cells_count, int pr_cells_shift) {
     double *previous_grid = (double *) malloc(sizeof(double) * (pr_cells_count + 2) * NY * NZ);
-    grid_init(grid, previous_grid, pr_cells_count, pr_cells_shift);
+    gridInit(grid, previous_grid, pr_cells_count, pr_cells_shift);
 
     int iteration_count = 0;
     double diff = 0;
@@ -160,10 +160,10 @@ int jacobi_method(double *grid, int pr_cells_count, int pr_cells_shift) {
     do {
         double pr_diff = 0;
 
-        send_grid_bound(previous_grid, pr_rank, comm_size, pr_cells_count, request_prev, request_next);
-        update_grid_center(grid, previous_grid, pr_cells_count, pr_cells_shift, &pr_diff);
-        recieve_grid_bound(pr_rank, comm_size, request_prev, request_next);
-        update_grid_bound(grid, previous_grid, pr_cells_count, pr_cells_shift, &pr_diff);
+        sendGridBound(previous_grid, pr_rank, comm_size, pr_cells_count, request_prev, request_next);
+        updateGridCenter(grid, previous_grid, pr_cells_count, pr_cells_shift, &pr_diff);
+        recieveGridBound(pr_rank, comm_size, request_prev, request_next);
+        updateGridBound(grid, previous_grid, pr_cells_count, pr_cells_shift, &pr_diff);
         MPI_Allreduce(&pr_diff, &diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
         iteration_count++;
 
@@ -181,7 +181,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &pr_rank);
 
     if (NX % comm_size != 0) {
-        std::cerr << ("Grid size should be multiple of number of processes\n");
+        std::cerr << ("Grid size should divide of number of processes\n");
         MPI_Finalize();
         return 0;
     }
@@ -191,13 +191,13 @@ int main(int argc, char *argv[]) {
 
     double *grid = (double *) malloc(sizeof(double) * (pr_cells_count + 2) * NY * NZ);
 
-    double t_start = MPI_Wtime();
-    int iteration_count = jacobi_method(grid, pr_cells_count, pr_cells_shift);
-    double t_end = MPI_Wtime();
+    double time_start = MPI_Wtime();
+    int iteration_count = jacobyMethod(grid, pr_cells_count, pr_cells_shift);
+    double time_end = MPI_Wtime();
 
     if (pr_rank == 0) {
         printf("Iteration count: %d\n", iteration_count);
-        printf("Time: %f\n", t_end - t_start);
+        printf("Time taken: %f\n", time_end - time_start);
     }
 
     free(grid);
