e571fbe291c01ba8a9ae0859b7e6c040298d0553
NickolAir <nikolay.ratushnyak03@gmail.com>
2023-05-04 12:15:47 +0700

    added delta calculation

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <mpi.h>
 #include <cmath>
 
-#define X0 -1
-#define X1 1
-#define Y0 -1
-#define Y1 1
-#define Z0 -1
-#define Z1 1
+constexpr int X0 = -1;
+constexpr int X1 = 1;
+constexpr int Y0 = -1;
+constexpr int Y1 = 1;
+constexpr int Z0 = -1;
+constexpr int Z1 = 1;
 
 //Главный параметр уравнения
 #define A 1e5
@@ -19,21 +19,21 @@
 #define FI0 0
 
 //Размеры сетки
-#define NX 320
-#define NY 320
-#define NZ 320
+#define NX 520
+#define NY 520
+#define NZ 520
 
 //Шаги сетки
 #define hx ((double)(X1-X0)/(NX-1))
 #define hy ((double)(Y1-Y0)/(NY-1))
 #define hz ((double)(Z1-Z0)/(NZ-1))
 
-#define I(i, j, k) ((i)*NY*NZ+(j)*NZ+(k))
+#define indx(i, j, k) ((i)*NY*NZ+(j)*NZ+(k))
 
 double fi(double i, double j, double k) {
     double x = X0 + (i) * hx;
-    double y = X0 + (j) * hy;
-    double z = X0 + (k) * hz;
+    double y = Y0 + (j) * hy;
+    double z = Z0 + (k) * hz;
     return x * x + y * y + z * z;
 }
 
@@ -50,14 +50,14 @@ void gridInit(double *grid, double *previous_grid, int pr_cells_count, int pr_ce
     for (int i = 1; i < pr_cells_count + 1; i++) {
         for (int j = 0; j < NY; j++) {
             for (int k = 0; k < NZ; k++) {
-                grid[I(i, j, k)] = FI0;
-                previous_grid[I(i, j, k)] = FI0;
+                grid[indx(i, j, k)] = FI0;
+                previous_grid[indx(i, j, k)] = FI0;
 
                 int actual_i = i + pr_cells_shift;
 
                 if (isBoundaryElement(actual_i, NX) || isBoundaryElement(j, NY) || isBoundaryElement(k, NZ)) {
-                    grid[I(i, j, k)] = fi(actual_i, j, k);
-                    previous_grid[I(i, j, k)] = fi(actual_i, j, k);
+                    grid[indx(i, j, k)] = fi(actual_i, j, k);
+                    previous_grid[indx(i, j, k)] = fi(actual_i, j, k);
                 }
             }
         }
@@ -66,9 +66,9 @@ void gridInit(double *grid, double *previous_grid, int pr_cells_count, int pr_ce
 
 double iterationFunc(int i, int j, int k, double *grid, int pr_cells_shift) {
     return ((1.0 / (2.0 / (hx * hx) + 2.0 / (hy * hy) + 2.0 / (hz * hz) + A)) *
-            ((grid[I(i + 1, j, k)] + grid[I(i - 1, j, k)]) / (hx * hx) +
-             (grid[I(i, j + 1, k)] + grid[I(i, j - 1, k)]) / (hy * hy) +
-             (grid[I(i, j, k + 1)] + grid[I(i, j, k - 1)]) / (hz * hz) -
+            ((grid[indx(i + 1, j, k)] + grid[indx(i - 1, j, k)]) / (hx * hx) +
+             (grid[indx(i, j + 1, k)] + grid[indx(i, j - 1, k)]) / (hy * hy) +
+             (grid[indx(i, j, k + 1)] + grid[indx(i, j, k - 1)]) / (hz * hz) -
              ro(i + pr_cells_shift, j, k)));
 }
 
@@ -76,7 +76,7 @@ double iterationFunc(int i, int j, int k, double *grid, int pr_cells_shift) {
 void updateGrid(int index, double *current_value, double *prev_value) {
     for (int j = 1; j < NY - 1; j++) {
         for (int k = 1; k < NZ - 1; k++) {
-            prev_value[I(index, j, k)] = current_value[I(index, j, k)];
+            prev_value[indx(index, j, k)] = current_value[indx(index, j, k)];
         }
     }
 }
@@ -87,9 +87,9 @@ void updateGridCell(int index, double *grid, double *previous_grid, int pr_cells
     if (!isBoundaryElement(actual_index, NX)) {
         for (int j = 1; j < NY - 1; j++) {
             for (int k = 1; k < NZ - 1; k++) {
-                grid[I(index, j, k)] = iterationFunc(index, j, k, previous_grid, pr_cells_shift);
+                grid[indx(index, j, k)] = iterationFunc(index, j, k, previous_grid, pr_cells_shift);
 
-                double cur_diff = fabs(grid[I(index, j, k)] - previous_grid[I(index, j, k)]);
+                double cur_diff = fabs(grid[indx(index, j, k)] - previous_grid[indx(index, j, k)]);
 
                 if (*pr_diff < cur_diff) {
                     *pr_diff = cur_diff;
@@ -106,6 +106,13 @@ void updateGridCenter(double *grid, double *previous_grid, int pr_cells_count, i
 
     updateGridCell(center, grid, previous_grid, pr_cells_shift, pr_diff);
 
+#if 1
+    for (int j = 2; j < pr_cells_count; j += 1) {
+        updateGridCell(j, grid, previous_grid, pr_cells_shift, pr_diff);
+    }
+#endif
+
+#if 0
     for (int j = 1; j < (pr_cells_count + 1) / 2; j++) {
         updateGridCell(center - j, grid, previous_grid, pr_cells_shift, pr_diff);
         updateGridCell(center + j, grid, previous_grid, pr_cells_shift, pr_diff);
@@ -114,6 +121,7 @@ void updateGridCenter(double *grid, double *previous_grid, int pr_cells_count, i
     if (pr_cells_count % 2 == 0) {
         updateGridCell(pr_cells_count, grid, previous_grid, pr_cells_shift, pr_diff);
     }
+#endif
 }
 
 // обновляет значения граничных элементов сетки
@@ -181,6 +189,24 @@ int jacobyMethod(double *grid, int pr_cells_count, int pr_cells_shift) {
     return iteration_count;
 }
 
+double accuracyEstimate(double *grid, int pr_cells_count, int pr_cells_shift) {
+    double accuracyEstimate = 0;
+    for (int i = 1; i < pr_cells_count + 1; i++) {
+        for (int j = 0; j < NY; j++) {
+            for (int k = 0; k < NZ; k++) {
+                int actual_i = i + pr_cells_shift;
+                double temp = abs(grid[indx(i, j, k)] - fi(actual_i, j, k));
+                if (temp > accuracyEstimate) {
+                    accuracyEstimate = temp;
+                }
+            }
+        }
+    }
+    double observational_error;
+    MPI_Reduce(&accuracyEstimate, &observational_error, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    return observational_error;
+}
+
 int main(int argc, char *argv[]) {
     int pr_rank, comm_size;
     MPI_Init(&argc, &argv);
@@ -202,9 +228,12 @@ int main(int argc, char *argv[]) {
     int iteration_count = jacobyMethod(grid, pr_cells_count, pr_cells_shift);
     double time_end = MPI_Wtime();
 
+    double delta = accuracyEstimate(grid, pr_cells_count, pr_cells_shift);
+
     if (pr_rank == 0) {
-        printf("Iteration count: %d\n", iteration_count);
+        printf("Iterations: %d\n", iteration_count);
         printf("Time taken: %f\n", time_end - time_start);
+        printf("Delta: %lf\n", delta);
     }
 
     free(grid);
